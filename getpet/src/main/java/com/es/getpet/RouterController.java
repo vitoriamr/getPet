d4b5093a7a0ff3add8f1afff66c7b6570605df32
brunomoutinho <brunoe.moutinho@gmail.com>
2017-05-28 14:24:18 -0300

    Servidor REST servindo templates corretamente.

--- getpet/src/main/java/com/es/getpet/RouterController.java
+++ getpet/src/main/java/com/es/getpet/RouterController.java
@@ -1,12 +1,13 @@
 package com.es.getpet;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class RouterController {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String index() {
 		return "index";
 	}
